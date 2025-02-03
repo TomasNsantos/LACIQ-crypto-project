95858aa9f23768eb22e7385db9bd1ed1d0bf81bd
Tomás Santos <tns@cin.ufpe.br>
2025-02-03 11:53:34 -0300

    fix: implementing e_k_c and testing main.py(unsucessfully)

--- ProjetoLACIQCriptografia/Chat/key_protocol.py
+++ ProjetoLACIQCriptografia/Chat/key_protocol.py
@@ -65,8 +65,10 @@ def exchange_keys_server(client_socket, p, g, s, enc_msg, conf_msg):
     
     # Calculate key
     key = get_key(S)
+
     # Decrypt message
     msg = decrypt_message(enc_msg, key)
+    
     # Send confirmation message
     send_json(client_socket, {"msg": conf_msg})
 
@@ -84,3 +86,27 @@ def exchange_keys_client(server_socket):
     g = server_data['g']
 
     # INSERT THE REST OF THE CODE HERE
+    # Generate private and public keys
+    a, A = create_keys(g, p)
+    # Send public key to server
+    send_json(server_socket, {"B": A})
+
+    # Receive server's public key
+    server_data = recv_json(server_socket)
+    B = server_data['B']
+
+    # Calculate shared key
+    S = pow(B, a, p)  # S = B^a mod p
+
+    # Calculate key
+    key = get_key(S)
+
+    # Receive and decrypt message
+    enc_msg = server_data['enc_msg']
+    msg = decrypt_message(enc_msg, key)
+
+    # Receive confirmation message
+    server_data = recv_json(server_socket)
+    conf_msg = server_data['msg']
+
+    return a, p, g, conf_msg
